f7390bf7b964553504c189b85f6fd4ae9a1dcb86
Wen Xiong Tu <csctwx@msn.com>
2015-08-26 23:09:36 -0400

    update

---
+++ AUG21/Gradient Animation Example/js/Simple-jQuery-Image-Slideshow-with-Ken-Burns-Effect/README.md
@@ -0,0 +1,18 @@
+# Kenburnsy 
+
+[![Build Status](https://travis-ci.org/ZeroOneStudio/kenburnsy.png?branch=master)](https://travis-ci.org/ZeroOneStudio/kenburnsy)
+[![Code Climate](https://codeclimate.com/github/ZeroOneStudio/kenburnsy.png)](https://codeclimate.com/github/ZeroOneStudio/kenburnsy)
+
+Simple JQuery plugin to make slideshows with Ken Burns effect.
+
+## Proof of concept. Work in progress!
+
+## Roadmap
+
+* Get rid of Velocity dependency
+* Pure CSS3 animations with fallback to simple slide changing
+* Reveal next slide just by changing z-index
+* Change structure to expose a simple API
+* Examples
+* Documentation
+* Get rid of jQuery dependency, target on modern browsers
---
+++ AUG21/Gradient Animation Example/js/Simple-jQuery-Image-Slideshow-with-Ken-Burns-Effect/gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+    header = require('gulp-header'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    csscomb = require('gulp-csscomb'),
+    prefix = require('gulp-autoprefixer'),
+    mochaPhantomJS = require('gulp-mocha-phantomjs'),
+    pkg = require('./package.json');
+
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
+
+gulp.task('jshint', function () {
+  var stream = gulp.src('./src/*.js')
+    .pipe(jshint({ lookup: true }))
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
+
+  return stream;
+});
+
+gulp.task('test', function () {
+  var stream = gulp.src('test/runner.html')
+    .pipe(mochaPhantomJS());
+  
+  return stream;
+});
+
+gulp.task('build', ['jshint'], function () {
+  // 
+  // Uglify and add banner to javascript files
+  // 
+  gulp.src('./src/*.js')
+    .pipe(header(banner, { pkg: pkg } ))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(uglify())
+    .pipe(header(banner, { pkg: pkg } ))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/'));
+
+  // 
+  // Autoprefix and add banner to css files
+  // 
+  gulp.src('./src/*.css')
+    .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+    .pipe(csscomb('zen'))
+    .pipe(header(banner, { pkg: pkg } ))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['jshint', 'test', 'build']);
+
+gulp.task('travis', ['jshint', 'test']);
